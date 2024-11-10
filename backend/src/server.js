a8daf743bba3ffa892016eabc4f140041027739a
Cláudio Roberto <claudioroberto0707@outlook.com>
2024-11-10 10:23:48 -0300

    projeto finalizado

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const routes = require('./routes');
-const cors = required('cors');
+const cors = require('cors');
 const app = express();
 
 app.use(express.json());
-app.use(routes);
 app.use(cors());
+app.use(routes);
+
 app.get('/', (req, res) => {
     res.send('Hello World');
 });
--- front_end/src/components/Login.js
+++ front_end/src/components/Login.js
@@ -1,22 +1,53 @@
-import React, { useState, axios } from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 
 function Login() {
 
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
+    const [user, setUser] = useState(null);
 
     const handleLogin = async (e) => {
         e.preventDefault();
         console.log(email, password);
 
-        const response = await axios.post('http://localhost:3000/login',
-            JSON.stringify({ email, password })
+        try {
 
-        );
-    }
+
+            const response = await axios.post('http://localhost:3000/login',
+                JSON.stringify({ email, password }),
+                {
+                    headers: { 'Content-Type': 'application/json' }
+
+                }
+
+            );
+            console.log(response.data);
+            setUser(response.data);
+
+        } catch (error) {
+            if (!error?.response) {
+                setError('Erro ao acessar o servidor');
+            } else if (error.response.status == 401) {
+                setError('Usuário ou senha Inválidos');
+            }
+        }
+    };
+const handleLogout = async (e) => {
+e.preventDefault();
+setUser(null);
+setError('');
+
+// senha no console.log para fins de Teste
+console.log('login contato@hotmail.com senha 123456')
+};
 
     return (
         <div className="login-form-wrap">
+            {user === null ? (
+                <div>
+
             <h2> Login</h2>
             <form className="login-form">
                 <input type='email' name='email' placeholder='Email' onChange={(e) => setEmail(e.target.value)} required />
@@ -24,11 +55,16 @@ function Login() {
                 <button type='submit' className="btn-login" onClick={(e) => handleLogin(e)}>Login</button>
 
             </form>
-
+            <p>{error}</p>
+            </div>
+            ) : ( 
+                <div>
+                    <h2> Olá, {user.name}</h2>
+                    <button type='button' className='btn-login' onClick={(e) => handleLogout(e)}>Logout</button>
         </div>
-
-
+        
+    )}
+      </div>
     );
-
 }
 export default Login;
